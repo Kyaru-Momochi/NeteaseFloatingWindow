1e00515f8645a602a6ad172ae59553fd2ab561d6
Kyaru_0 <133975271+Kyaru-Momochi@users.noreply.github.com>
2026-06-02 17:00:27 +0800

    Add files via upload

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,1 +1,32 @@
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
+import type { TrackInfo, AppConfig } from '../shared/types';
 
+contextBridge.exposeInMainWorld('electronAPI', {
+  onTrackChanged(callback: (track: TrackInfo) => void): () => void {
+    const handler = (_event: IpcRendererEvent, track: TrackInfo) => callback(track);
+    ipcRenderer.on('track-changed', handler);
+    return () => ipcRenderer.removeListener('track-changed', handler);
+  },
+
+  onConfigChanged(callback: (config: AppConfig) => void): () => void {
+    const handler = (_event: IpcRendererEvent, config: AppConfig) => callback(config);
+    ipcRenderer.on('config-changed', handler);
+    return () => ipcRenderer.removeListener('config-changed', handler);
+  },
+
+  getConfig(): Promise<AppConfig> {
+    return ipcRenderer.invoke('get-config');
+  },
+
+  saveConfig(config: Partial<AppConfig>): Promise<AppConfig> {
+    return ipcRenderer.invoke('save-config', config);
+  },
+
+  resetConfig(): Promise<AppConfig> {
+    return ipcRenderer.invoke('reset-config');
+  },
+
+  openConfig(): void {
+    ipcRenderer.send('open-config');
+  },
+});
